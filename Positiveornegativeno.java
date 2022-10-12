344df482919ebeaa4ea6960b7c9b50c058faee24
Balasaraswathi-26 <115528028+Balasaraswathi-26@users.noreply.github.com>
2022-10-13 00:19:34 +0530

    Add files via upload
    week1.day3

---
+++ Positiveornegativeno.java
@@ -0,0 +1,13 @@
+package week1.day3;
+
+public class Positiveornegativeno 
+{
+	public static void main (String[] args)
+	{
+		int n=-26;
+		if(n<0)
+			System.out.println("Negative");
+		else
+			System.out.println("Positive");
+	}
+}
---
+++ fibo.java
@@ -0,0 +1,18 @@
+package week1.day3;
+
+public class fibo{
+	public static void main(String[] args) {
+		int n=11;
+		int k=1,t=1,m;
+		System.out.println(k);
+		System.out.println(t);
+		for(int i=3;i<=n;i++)
+		{
+			m=k+t;
+			System.out.println(m);
+			k=t;
+			t=m;
+		}
+		}
+	
+}
